343e9c46c463ec0f6eb96d4bc8d4eb22a7414fea
V-code-japan <minecraft@e-sia.jp>
2025-11-27 13:19:43 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
 player.onChat("run", function () {
-	
+    blocks.fill(
+        PLANKS_BIRCH,
+        world(23, 72, 117),
+        world(30, 77, 125),
+        FillOperation.Hollow
+    )
+    blocks.place(OAK_DOOR, world(23, 73, 121))
+    blocks.fill(
+        GLASS,
+        world(25, 74, 117),
+        world(28, 75, 117),
+        FillOperation.Replace
+    )
 })
